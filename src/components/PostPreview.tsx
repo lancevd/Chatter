b632029d6c0aa4daff63651e1d6a70b8c51c9113
lancevd <olamosob@gmail.com>
2023-06-28 14:14:30 +0100

    Post Details Done

--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -32,7 +32,7 @@ const PostPreview: React.FC<postProps> = ({post}) => {
             </div>
             <div className="flex flex-col">
                     <h5>{authorData ? authorData.name : ''}</h5>
-                <p><span>{authorData ? authorData.occupation : ''} </span> |{post.data ? new Date(post.data.publishedOn).toLocaleString('en-Us') : ''} </p>
+                <p><span>{authorData ? authorData.occupation : ''} </span> | {post.data ? new Date(post.data.publishedOn).toLocaleString('en-Us', {day: 'numeric', month: 'long', year: 'numeric'} ) : ''} </p>
             </div>
         </div>
         <br />
--- src/pages/PostDetails.tsx
+++ src/pages/PostDetails.tsx
@@ -19,11 +19,11 @@ const PostDetails: React.FC<articleProps> = ({post, author}) => {
             </div>
             <div className="flex flex-col">
                     <h5>{author?.data?.name}</h5>
-                <p><span>{author?.data?.occupation} </span> | </p>
+                <p><span>{author?.data?.occupation} </span> | {post.data ? new Date(post.data.publishedOn).toLocaleString('en-Us', {day: 'numeric', month: 'long', year: 'numeric'} ) : ''}</p>
             </div>
         </div>
         <br />
-        <h2>{post.data ? post.data.title : 'Loading...'}</h2>
+        <h2>{post.data ? post?.data?.title : 'Loading...'}</h2>
         <br />
         <img src={post.data ? post.data.bannerImage : 'Loading'} alt="" className='w-full' />
         <br />
@@ -31,15 +31,15 @@ const PostDetails: React.FC<articleProps> = ({post, author}) => {
         <br />
         <div className="flex justify-between">
             <div>
-                <div className='flex gap-2 items-center'><span><AiOutlineComment/> </span> {post.data.comments} </div>
+                <div className='flex gap-2 items-center'><span><AiOutlineComment/> </span> {post?.data?.comments} </div>
             </div>
             <div className="h-3"></div>
             <div>
-                <div className='flex gap-2 items-center'><span><AiOutlineHeart/> </span> {post.data.likes} </div>
+                <div className='flex gap-2 items-center'><span><AiOutlineHeart/> </span> {post?.data?.likes} </div>
             </div>
             <div className="h-3"></div>
             <div>
-                <div className='flex gap-2 items-center'><span><MdOutlineAnalytics/> </span> {post.data.views} views </div>
+                <div className='flex gap-2 items-center'><span><MdOutlineAnalytics/> </span> {post?.data?.views} views </div>
             </div>
         </div>
     </div>
