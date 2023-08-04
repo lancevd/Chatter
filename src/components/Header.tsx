df7313db46b767332569ad4838ee92a770f83989
lancevd <olamosob@gmail.com>
2023-08-04 04:25:05 +0100

    Auth State Tracking added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,20 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import {FaSearch } from 'react-icons/fa'
 import {MdOutlineNotificationsNone} from 'react-icons/md'
 import { Link } from 'react-router-dom'
 import { ChatterContext } from '../context/ChatterContext'
 
 const Header = () => {
-   const {currentUser} = useContext(ChatterContext)
+   const {logOut} = useContext(ChatterContext)
+   const [showBtn, setShowBtn] = useState('hidden')
+
+   function toggleBtn () {
+      if (showBtn == 'hidden') {
+         setShowBtn('block')
+      } else {
+         setShowBtn('hidden')
+      }
+   }
   return (
     <header className='w-full md:w-[75%] lg:w-[80%] border absolute right-0 h-16 py-1 px-8 lg:px-20'>
         <div className='flex justify-end gap-12 md:gap-32'>
@@ -15,8 +24,13 @@ const Header = () => {
            </div>
            <div className='flex items-center gap-4 text-2xl'>
               <MdOutlineNotificationsNone/>
-              {currentUser ? 
-              (<div className="w-8 h-8 md:w-12 md:h-12 rounded-full bg-blue-800"></div>)
+              {window.localStorage.getItem('ACTIVE_USER') ? 
+              (
+               <div className='flex flex-col'>
+                  <div onClick={toggleBtn} className="w-8 h-8 md:w-12 md:h-12 cursor-pointer rounded-full bg-blue-800"></div>
+                  <button onClick={logOut} className={`bg-blue-800 p-1 text-white border rounded-md text-sm ${showBtn}`}>Logout</button>
+               </div>
+              )
               :
               (<Link to={'/register'}><div className="btn-pry w-fit h-fit text-xs md:text-base p-1 md:p-2 cursor-pointer">Sign up</div></Link>)
               }
--- src/context/ChatterContext.js
+++ src/context/ChatterContext.js
@@ -2,7 +2,7 @@ import  { useState, useEffect, createContext } from "react";
 import {collection, getDocs, setDoc, doc} from 'firebase/firestore'
 import {db, auth, googleProvider, createWithMail, signInWihMail} from '../firebase'
 import { MdAod } from "react-icons/md";
-import { signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { signInWithPopup, onAuthStateChanged, signOut } from "firebase/auth";
 
 const ChatterContext = createContext()
 
@@ -10,6 +10,7 @@ const ChatterProvider = ({children}) => {
     const [users, setUsers] = useState([])
     const [posts, setPosts] = useState([])
     const [currentUser, setCurrentUser] = useState(null)
+    const [userActive, setUserActive] = useState(true)
 
     useEffect(()=> {
         const getUsers = async () => {
@@ -27,6 +28,12 @@ const ChatterProvider = ({children}) => {
 
         getUsers()
     },[])
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (activeUser) => {
+            setCurrentUser(activeUser)
+        })
+    })
 
     useEffect(()=> {
         const getPosts = async () => {
@@ -83,15 +90,45 @@ const ChatterProvider = ({children}) => {
         window.location.href = '/feed'
     }
 
-    useEffect(() => {  
-        console.log(currentUser)
-    }, [currentUser])
+    useEffect(() => {
+        // Check if there is a user object in the local storage
+        const activeUser = window.localStorage.getItem("ACTIVE_USER");
+        if (activeUser) {
+          setCurrentUser(JSON.parse(activeUser));
+        }
+      }, []);
     
+      useEffect(() => {
+        // Add a listener to update the user state when the authentication state changes
+        const unsubscribe = onAuthStateChanged(auth, (activeUser) => {
+          if (activeUser) {
+            // Save the user object in the local storage when the user is logged in
+            window.localStorage.setItem("ACTIVE_USER", JSON.stringify(activeUser));
+          } else {
+            // Remove the user object from the local storage when the user is logged out
+            window.localStorage.removeItem("ACTIVE_USER");
+          }
+          setCurrentUser(activeUser);
+        });
+    
+        // Clean up the listener when the component is unmounted
+        return () => unsubscribe();
+    },[])
+    
+    const logOut = () => {
+        signOut(auth).then(() => {
+            window.location.href = '/feed'
+          }).catch((error) => {
+            console.log(error);
+            
+          });
+        window.localStorage.removeItem('ACTIVE_USER')
+    }
 
 
     return (
         <ChatterContext.Provider 
-        value={{posts,users, handleGoogleAuth, currentUser}}>
+        value={{posts,users, handleGoogleAuth, currentUser, logOut}}>
             {children}
         </ChatterContext.Provider>
     )
